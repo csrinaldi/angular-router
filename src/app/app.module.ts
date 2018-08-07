d942a44f2f72353036b83c1a0638a63c4f6b3ae2
Cristian Rinaldi <csrinaldi@gmail.com>
2018-08-07 20:17:45 -0300

    test load router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {AppComponent} from './app.component';
+import { AppComponent } from "./app.component";
 
-import {ProductListComponent} from './product-list/product-list.component';
+import { ProductListComponent } from "./product-list/product-list.component";
 
-import {ProductListSidebarComponent} from './product-list-sidebar/product-list-sidebar.component';
+import { ProductListSidebarComponent } from "./product-list-sidebar/product-list-sidebar.component";
 
-import {ProductDetailComponent} from './product-detail/product-detail.component';
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
-import {SidebarComponent} from './sidebar/sidebar.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
-import {routingModule} from './routing.module';
-import {TestAsyncModule} from './test-async/test.async.module';
+import { routingModule } from "./routing.module";
+import { TestAsyncModule } from "./test-async/test.async.module";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ProductListComponent,
-        ProductDetailComponent,
-        SidebarComponent,
-        ProductListSidebarComponent
-    ],
-    imports: [BrowserModule, TestAsyncModule, routingModule],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    SidebarComponent,
+    ProductListSidebarComponent
+  ],
+  imports: [BrowserModule, routingModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/test-async/test.async.module.ts
+++ src/app/test-async/test.async.module.ts
@@ -1,19 +1,12 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {routes} from './test.async.routers';
-import {TAsyncComponent} from './test-async-component/t.async.component';
-import {TestAsyncService} from './test.async.service';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { routes } from "./test.async.routers";
+import { TAsyncComponent } from "./test-async-component/t.async.component";
+import { TestAsyncService } from "./test.async.service";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        // routes
-    ],
-    declarations: [TAsyncComponent],
-    providers: [
-        TestAsyncService
-    ]
+  imports: [CommonModule, routes],
+  declarations: [TAsyncComponent],
+  providers: [TestAsyncService]
 })
 export class TestAsyncModule {}
